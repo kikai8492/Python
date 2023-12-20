62c2b7704a6e3c7528168d2ebed8dff1808e6bba
shuto kikai <yysskikai04@icloud.com>
2023-12-20 10:04:14 +0900

    img

--- index.py
+++ index.py
@@ -1,19 +1,23 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 st.title('Streamlit 入門')
 
-st.write('DataFrame')
+st.write('Display Image')
 
-df = pd.DataFrame( 
-    np.random.rand(100,2),
-    columns=['lat','lon']
-)
+img = Image.open('cat2.jpg')
+
+st.image(img, caption='cat', use_column_width=True)
+# df = pd.DataFrame( 
+#     np.random.rand(100,2)/[50,50] + [35.69,139.70],
+#     columns=['lat','lon']
+# )
 # df
-# st.dataframe(df.style.highlight_max(axis=0))
+# # st.dataframe(df.style.highlight_max(axis=0))
 
-# """
-# # markdown
-# """
-st.bar_chart(df) # 折れ線グラフ
+# # """
+# # # markdown
+# # """
+# st.map(df) # 折れ線グラフ
